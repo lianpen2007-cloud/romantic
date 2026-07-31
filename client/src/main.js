5b0587e73c63da05719b6fbf3caa69db481e889f
lianpen <lianpen2007@gmail.com>
2026-08-01 02:25:25 +0800

    .

--- client/src/main.js
+++ client/src/main.js
@@ -1,19 +1,17 @@
 import Vue from 'vue'
-import App from './App.vue'
-
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 import VueRouter from 'vue-router'
 Vue.use(VueRouter)
+
+import App from './App.vue'
 import routerConfig from './router'
-const router = new VueRouter(routerConfig)
-
 new Vue({
-  router,
+  router: new VueRouter(routerConfig),
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#index-html-body')
 
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
  
-Vue.use(ElementUI);
